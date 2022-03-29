e9df5ee920db76eb5970250285e2a769ed69528a
antonio.gonzalez <antonio.gonzalez@olx.com>
2022-03-29 18:06:24 +0200

    MISC - Included dependencies and packages to be used in the app

--- app/src/main/java/com/example/giffmedanextone/feature_item/presentation/MainActivity.kt
+++ app/src/main/java/com/example/giffmedanextone/feature_item/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.giffmedanextone
+package com.example.giffmedanextone.feature_item.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -16,15 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GiffMeDaNextOneTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Greeting(name = "Steamer")
         }
     }
 }
