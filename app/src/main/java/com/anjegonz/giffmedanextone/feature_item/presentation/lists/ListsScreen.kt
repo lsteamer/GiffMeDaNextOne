538f9cae3f17a21a64cb79afd608443a3efefe4a
Antonio Gonzalez <lsteamer@gmail.com>
2022-08-29 15:33:53 +0200

    Added swipeToDismiss deletions

--- app/src/main/java/com/anjegonz/giffmedanextone/feature_item/presentation/lists/ListsScreen.kt
+++ app/src/main/java/com/anjegonz/giffmedanextone/feature_item/presentation/lists/ListsScreen.kt
@@ -1,20 +1,25 @@
 package com.anjegonz.giffmedanextone.feature_item.presentation.lists
 
+import android.util.Log
 import androidx.compose.animation.*
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Sort
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.anjegonz.giffmedanextone.R
@@ -25,7 +30,7 @@ import com.anjegonz.giffmedanextone.feature_item.presentation.lists.components.S
 import com.anjegonz.giffmedanextone.feature_item.presentation.util.Screen
 import kotlinx.coroutines.flow.collectLatest
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterialApi::class)
 @Composable
 fun ListsScreen(
     navController: NavController,
@@ -101,26 +106,76 @@ fun ListsScreen(
             }
             Spacer(modifier = Modifier.height(16.dp))
             LazyColumn(modifier = Modifier.fillMaxSize()) {
-                items(state.lists) { list ->
-                    SingleListItem(
-                        singleList = list,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .combinedClickable(onClick = {
+
+
+                items(
+                    items = state.lists,
+                    key = { list ->
+                        list.hashCode()
+                    }
+                ) { list ->
+
+                    val dismissState = rememberDismissState(
+                        confirmStateChange = {
+                            if (it == DismissValue.DismissedToStart) {
                                 viewModel.onEvent(
-                                    ListsEvent.GiffMeDaNextOne(
-                                        list
-                                    )
+                                    ListsEvent.DeleteList(list)
                                 )
-                            }, onLongClick = {
-                                navController.navigate(
-                                    Screen.AddEditSingleListScreen.route +
-                                            "?listId=${list.id}&listColor=${list.color}"
-                                )
-                            })
+                            }
+                            true
+                        }
                     )
+
+                    SwipeToDismiss(
+                        state = dismissState,
+                        background = {
+                            val color = when (dismissState.dismissDirection) {
+                                DismissDirection.StartToEnd -> Color.Transparent
+                                DismissDirection.EndToStart -> Color.Red
+                                null -> Color.Transparent
+                            }
+
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .background(color)
+                                    .padding(8.dp)
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Delete,
+                                    contentDescription = null,
+                                    tint = Color.White,
+                                    modifier = Modifier.align(Alignment.CenterEnd)
+                                )
+                            }
+
+                        },
+                        directions = setOf(DismissDirection.EndToStart)
+                    ) {
+                        SingleListItem(
+                            singleList = list,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .combinedClickable(onClick = {
+                                    viewModel.onEvent(
+                                        ListsEvent.GiffMeDaNextOne(
+                                            list
+                                        )
+                                    )
+                                }, onLongClick = {
+                                    navController.navigate(
+                                        Screen.AddEditSingleListScreen.route +
+                                                "?listId=${list.id}&listColor=${list.color}"
+                                    )
+                                })
+                        )
+                    }
+
+
                     Spacer(Modifier.height(16.dp))
+
                 }
+
             }
         }
     }
